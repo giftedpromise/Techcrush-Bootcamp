0e3005cefdb164a83a2ca7ce815c081f375190e2
giftedpromise <giftedpromise1992@gmail.com>
2024-10-13 02:38:14 +0100

    first class

---
+++ Week one/classOne.js
@@ -0,0 +1,18 @@
+/* 
+Introduction to Node.Js
+
+What is Backend Development?
+First, what is Backend Development?
+Backend development refers to the server-side of an application and everything that communicates 
+between the database and the browser (frontend).
+Think of the backend as the engine under the hood of a car — it is essential for
+ functionality but not always visible.
+
+ Responsibilities
+1. Server Logic: Handling business logic and application workflows.
+2. Database Management: CRUD operations (Create, Read, Update, Delete) with databases.
+3.Authentication and Security: User authentication, data protection, role-based access control and secure communication.
+
+
+
+*/
