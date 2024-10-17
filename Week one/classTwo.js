3de8068c3635c11e85b12db6ac6a1b20afe806ad
giftedpromise <giftedpromise1992@gmail.com>
2024-10-17 11:46:37 -1200

    classtwo

--- Week one/classTwo.js
+++ Week one/classTwo.js
@@ -18,3 +18,27 @@ let myAge = 5;
 let my_name = "Abraham";
 // const - constant, cannot be reassigned
 const pi = 3.14;
+
+//2. Data Types
+// Number: Represents both integers and floating-point numbers
+let age = 25; // this is an integer
+let price = 19.99; // This is a floating number or decimal
+// String: Text data enclosed in single, double, or template literals
+let name = "John"; //I used double quotes
+let country = "Rwanda"; //I used single quotes
+let pet = `dog`; //I used backticks
+let greetingPet = "Hello" + pet; // Concatenation
+let greetingName = `Hello, ${name}!`; // Template literal
+// Boolean: true or false
+let isAdult = true;
+let isChild = false;
+
+// Null: Represents an intentional absence of a value
+let value = null;
+// Undefined: A variable declared but not yet assigned a value
+let home;
+console.log(home); // undefined
+// Object: Key-value pairs, used for storing structured data
+let person = { name: "Alice", age: 30 };
+// Array: Ordered collection of values
+let numbers = [1, 2, 3, 4];
