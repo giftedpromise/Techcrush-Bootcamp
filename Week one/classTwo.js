d40b27143ce723da1505212bbca995a54a0e2eda
giftedpromise <giftedpromise1992@gmail.com>
2024-10-17 11:42:37 -1200

    classtwo

---
+++ Week one/classTwo.js
@@ -0,0 +1,20 @@
+/*
+Introduction to Backend Javascript 2
+Understanding javascript in Node.Js
+Best Practices for Naming Variables (Conventions)
+Use descriptive names and valid characters. It must start with a letter (a-z or A-Z), underscore (_), or dollar sign ($). 
+Use camelCase or snake_case
+Avoid single-letter variable names
+Use const for constants
+Variables are case-sensitive: This means myVariable and myvariable are different variables.
+You cannot use reserved keywords as variable names. Reserved keywords are words like let, const, function, if, else, etc.
+
+
+*/
+
+//1. Variables and Constants
+// let - block-scoped variable (Preferred)
+let myAge = 5;
+let my_name = "Abraham";
+// const - constant, cannot be reassigned
+const pi = 3.14;
