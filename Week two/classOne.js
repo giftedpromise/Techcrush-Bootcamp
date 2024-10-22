4161d34bf0b73b5b007e228429d7db6e6ac1b4d6
giftedpromise <giftedpromise1992@gmail.com>
2024-10-22 21:35:09 +0100

    week two

--- Week two/classOne.js
+++ Week two/classOne.js
@@ -9,5 +9,28 @@ and manage project dependencies.
 Why is it important?
 It simplifies the process of installing, updating, and managing third-party libraries and tools.
 
+Brief history
+Created by Isaac Z. Schlueter in 2010.
+Became the official package manager for Node.js in 2011.
+Now maintained by npm, Inc., a subsidiary of GitHub (owned by Microsoft).
+
+# Initialize a new Node.js project
+npm init
+# Install a package and add to dependencies
+npm install package-name
+# Install a specific version of a package
+npm install package-name@version
+# Install a package as a dev dependency
+npm install package-name --save-dev
+# Uninstall a package
+npm uninstall package-name
+# Update packages
+npm update
+# List installed packages
+npm list
+# Run a script defined in package.json
+npm run script-name
+
+
 
 */
