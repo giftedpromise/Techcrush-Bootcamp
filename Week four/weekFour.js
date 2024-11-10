ecb234fa9dba9eabd72f7e6e1acef790e6e831b3
giftedpromise <giftedpromise1992@gmail.com>
2024-11-10 22:11:51 +0100

    Week three

---
+++ Week four/weekFour.js
@@ -0,0 +1,41 @@
+/*
+Backend Development:Node.js HTTP and FS Modules, and Introduction to Express.Js
+Node.js HTTP Module
+Getting started with servers
+
+A URL (Uniform Resource Locator) is the web address used to locate resources on the internet. It is essential for
+accessing websites, APIs, and other services. URLs are structured to convey the necessary information for identifying
+and locating a specific resource. Now, let’s talk about components of a URL
+
+Here's an example URL: https://www.example.com:8080/path/to/resource?query=search#section
+
+Protocol (https://):
+
+Specifies the protocol used for the connection, such as http, https, ftp, etc.
+
+https stands for Hypertext Transfer Protocol Secure, which is an encrypted version of http.
+
+Host (www.example.com):
+
+The domain name or IP address of the server hosting the resource.
+
+The www is a subdomain, while example.com is the main domain.
+
+Port (:8080):
+
+An optional part that specifies the port number on the server for the connection.
+
+Common ports are 80 for HTTP and 443 for HTTPS; if these are used, they are typically omitted in the URL.
+
+Path (/path/to/resource):
+
+Indicates the specific path to a resource on the server.
+
+It follows the domain and is structured like directories or folders.
+
+Query String (?query=search):
+
+Contains data passed to the server, typically in key-value pairs (key=value format).
+
+Multiple parameters are separated by &, e.g., ?key1=value1&key2=value2.
+*/
